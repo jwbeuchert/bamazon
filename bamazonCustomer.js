4b0b438bbdf060b517179d904eb69f24f6abe5c5
Jacob Beuchert <jacobbeuchert@gmail.com>
2019-11-07 23:45:48 -0700

    Adding code to get bamazon to work properly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,44 +1,121 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "root",
   database: "bamazon"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
-});
-// function which prompts the user for what action they should take
-function start() {
-  inquirer
-    .prompt({
-      name: "idAndQuantity",
-      type: "list",
-      message: "What item [ID] and [QUANTITY] would you like?",
-      choices: ["ID", "QUANTITY", "EXIT"]
-    })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.idAndQuantity === "POST") {
-        idAuction();
-      }
-      else if(answer.idAndQuantity === "BID") {
-        quantityAuction();
-      } else{
-        connection.end();
-      }
+var products;
+var runFirstTime = true;
+
+function getProducts() {
+    var queryProducts = 'SELECT * FROM products';
+    connection.query(queryProducts, function(error, results) {
+        if (error) {
+            console.log('Connection error occured!');
+        } else {
+            products = results;
+        }
+        if (runFirstTime) {
+            getListofProducts()
+            runFirstTime = false;
+        }
     });
 }
+
+function getListofProducts() {
+    console.log('\n')
+    // table headers
+    console.log('ID:\t|\tProduct      \t|\tPrice')
+    products.forEach(product => {
+        var divider = '\t|\t';
+        var lg_divider = '     \t|\t';
+        var list = "";
+        if (product.product_name.length < 8) {
+            list = product.item_id +  divider + product.product_name + lg_divider + '$' + product.price.toFixed(2);
+        } else if (product.product_name.length > 11) {
+            list = product.item_id +  divider + product.product_name.substring(0, 11) + divider + '$' + product.price.toFixed(2);
+        } else {
+            list = product.item_id +  divider + product.product_name + divider + '$' + product.price.toFixed(2);
+        }
+        
+        if (product.stock_quantity > 0) {
+            if (product.item_id % 2 === 0) {
+                console.log(list);
+            } else {
+                console.log(list);
+            }
+        } else {
+            console.log(list + ' OUT OF STOCK');
+        }
+    });
+
+    sellProducts();
+}
+
+var updateProducts = function(product_name, amount_purchased, cost,) {
+    var queryProducts = 'UPDATE products SET stock_quantity = stock_quantity - ?';
+    connection.query(queryProducts, [amount_purchased, { product_name: product_name }], function(error, results) {
+        if (error) throw error; 
+        console.log('You purchased ' + product_name + ' at a quantity of ' + amount_purchased +
+            '.\nYour total amount charged is $' + cost.toFixed(2) + '.' );
+        getProducts();
+        buyAgain();
+    });
+}
+
+function sellProducts() {
+        inquirer.prompt([
+            {
+                name: 'prod_id',
+                message: ('What is the ID of the product you want to buy?'),
+                validate: function(value) {
+                    if (/[0-9]/g.test(value) && value > 0 && value <= products.length) {
+                        if (products[value-1].stock_quantity < 1) {
+                            return 'Out of stock! Choose another item!';
+                        }
+                        return true;
+                    }
+                    return 'Enter a valid ID!';
+                }
+            }
+        ]).then(function(id_answer) {
+            console.log('\nYou selected ' + products[id_answer.prod_id-1].product_name + '. There are ' + products[id_answer.prod_id-1].stock_quantity + ' available to purchase.');
+            inquirer.prompt(
+                {
+                    name: 'quantity',
+                    message: ('How many do you wish to buy?'),
+                    validate: function(value) {
+                        if (/[0-9]/g.test(value) && value > 0 && value <= products[id_answer.prod_id-1].stock_quantity) {
+                            return true;
+                        }
+                        return ('Enter in a valid stock quantity');
+                    }
+                }).then(function(quantity_answer) {
+                    var cost = products[id_answer.prod_id-1].price * quantity_answer.quantity;
+                    updateProducts(products[id_answer.prod_id-1].product_name, quantity_answer.quantity, cost,);
+            });
+        });    
+}
+
+function buyAgain() {
+    inquirer.prompt(
+        {
+            name: 'again',
+            message: 'Do you want to purchase another item?',
+            type: 'confirm'
+        }
+    ).then(function(answer) {
+        if (answer.again) {
+            getListofProducts();
+        } else {
+            connection.end();
+        }
+    });
+}
+
+getProducts();
